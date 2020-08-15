84c0ab4fd0f75c3bbec5ca55cb8ff972b5b2df1f
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2020-08-15 22:20:30 +0100

    Added inline documentation, extended tests

--- PutridParrot.Randomizer/RandomizerExtensions.cs
+++ PutridParrot.Randomizer/RandomizerExtensions.cs
@@ -7,45 +7,124 @@ namespace PutridParrot.Randomizer
     /// </summary>
     public static class RandomizerExtensions
     {
+        /// <summary>
+        /// Returns a random integer greater or equal to 0 and less
+        /// than int.MaxValue
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
         public static int NextInt(this IRandomizer r) => r.NextInt(0, int.MaxValue);
-
+        /// <summary>
+        /// Returns a random integer greater or equal to 0 and less
+        /// than the supplied maxValue
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
         public static int NextInt(this IRandomizer r, int maxValue) => r.NextInt(0, maxValue);
-
+        /// <summary>
+        /// Returns a random double greater or equal to 0 and less
+        /// than double.MaxValue
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
         public static double NextDouble(this IRandomizer r) => r.NextDouble(0, double.MaxValue);
+        /// <summary>
+        /// Returns a random double greater or equal to 0 and less
+        /// than the supplied maxValue
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
         public static double NextDouble(this IRandomizer r, double maxValue) => r.NextDouble(0, maxValue);
-
+        /// <summary>
+        /// Returns a random double greater or equal to 0 and less
+        /// than long.MaxValue
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
         public static long NextLong(this IRandomizer r) => r.NextLong(0, long.MaxValue);
-
+        /// <summary>
+        /// Returns a random double greater or equal to 0 and less
+        /// than the supplied maxValue
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
         public static long NextLong(this IRandomizer r, long maxValue) => r.NextLong(0, maxValue);
-
-        public static long NextLong(this IRandomizer r, long minValue, long maxValue)
-        {
-            var value = (maxValue * 1.0 - minValue * 1.0) * r.NextDouble(0, 1) + minValue * 1.0;
-            return Convert.ToInt64(value);
-        }
-
+        /// <summary>
+        /// Returns a random double greater or equal to the supplied minValue and less
+        /// than the supplied maxValue
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public static long NextLong(this IRandomizer r, long minValue, long maxValue) =>
+            Convert.ToInt64((maxValue * 1.0 - minValue * 1.0) * r.NextDouble(0, 1) + minValue * 1.0);
+        /// <summary>
+        /// Returns a random boolean
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
         public static bool NextBool(this IRandomizer r) => r.NextInt(2) == 1;
-
-        public static DateTime NextDateTime(this IRandomizer r)
-        {
-            return r.NextDateTime(DateTime.Now, DateTime.MaxValue);
-        }
-
-        public static DateTime NextDateTime(this IRandomizer r, DateTime endDateTime)
-        {
-            var now = DateTime.Now;
-            return r.NextDateTime(now, endDateTime);
-        }
-
-        public static DateTime NextDateTime(this IRandomizer r, DateTime startDateTime, DateTime endDateTime)
-        {
-            var next = r.NextInt((endDateTime - startDateTime).Days);
-            return startDateTime.AddDays(next);
-        }
-
+        /// <summary>
+        /// Returns a random DateTime greater or equal to DateTime.Now and less than
+        /// DateTime.MaxValue
+        /// </summary>
+        /// <param name="r"></param>
+        /// <returns></returns>
+        public static DateTime NextDateTime(this IRandomizer r) => r.NextDateTime(DateTime.Now, DateTime.MaxValue);
+        /// <summary>
+        /// Returns a random DateTime greater or equal to DateTime.Now and less than
+        /// the supplied endDateTime
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="endDateTime"></param>
+        /// <returns></returns>
+        public static DateTime NextDateTime(this IRandomizer r, DateTime endDateTime) => r.NextDateTime(DateTime.Now, endDateTime);
+        /// <summary>
+        /// Returns a random DateTime greater or equal to the supplied startDateTime and less than
+        /// the supplied endDateTime
+        /// </summary>
+        /// <param name="r"></param>
+        /// <param name="startDateTime"></param>
+        /// <param name="endDateTime"></param>
+        /// <returns></returns>
+        public static DateTime NextDateTime(this IRandomizer r, DateTime startDateTime, DateTime endDateTime) => 
+            startDateTime.AddDays(r.NextInt((endDateTime - startDateTime).Days));
+        /// <summary>
+        /// Returns a random item from an array from index equal to or greater
+        /// than 0 index and less than the length of the array. If no array is supplied
+        /// or if the index is outside of the range if the array, default(T) is return
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="r"></param>
+        /// <param name="items"></param>
+        /// <returns></returns>
         public static T NextItem<T>(this IRandomizer r, T[] items) => r.NextItem(items, 0, items?.Length ?? 0);
+        /// <summary>
+        /// Returns a random item from an array from index equal to or greater
+        /// than 0 index and less than the maxIndex. If no array is supplied
+        /// or if the index is outside of the range if the array, default(T) is return
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="r"></param>
+        /// <param name="items"></param>
+        /// <param name="maxIndex"></param>
+        /// <returns></returns>
         public static T NextItem<T>(this IRandomizer r, T[] items, int maxIndex) => r.NextItem(items, 0, maxIndex);
-
+        /// <summary>
+        /// Returns a random item from an array from the index equal to or greater
+        /// than minIndex index and less than the maxIndex. If no array is supplied
+        /// or if the index is outside of the range if the array, default(T) is return
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="r"></param>
+        /// <param name="items"></param>
+        /// <param name="minIndex"></param>
+        /// <param name="maxIndex"></param>
+        /// <returns></returns>
         public static T NextItem<T>(this IRandomizer r, T[] items, int minIndex, int maxIndex)
         {
             if (items == null || items.Length == 0)
---
+++ Tests.PutridParrot.Randomizer/RandomizerExtensionTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using NUnit.Framework;
+using PutridParrot.Randomizer;
+using Telerik.JustMock;
+
+namespace Tests.PutridParrot.Randomizer
+{
+    public class RandomizerExtensionTests
+    {
+        [Test]
+        public void NextBool_IfNextInt0_ExpectFalse()
+        {
+            var mock = Mock.Create<IRandomizer>();
+
+            Mock.Arrange(() => mock.NextInt(0, 2)).Returns(0);
+
+            Assert.IsFalse(mock.NextBool());
+        }
+
+        [Test]
+        public void NextBool_IfNextInt1_ExpectTrue()
+        {
+            var mock = Mock.Create<IRandomizer>();
+
+            Mock.Arrange(() => mock.NextInt(0, 2)).Returns(1);
+
+            Assert.IsTrue(mock.NextBool());
+        }
+
+        [Test]
+        public void NextDateTime_WithKnownValue_ExpectCorrespondingDateTime()
+        {
+            var mock = Mock.Create<IRandomizer>();
+
+            var startDate = DateTime.Now.Date;
+            var endDate = startDate.AddDays(5);
+
+            Mock.Arrange(() => mock.NextInt(0, 5)).Returns(2);
+
+            Assert.AreEqual(startDate.AddDays(2).Date, mock.NextDateTime(startDate, endDate));
+        }
+
+        [Test]
+        public void NextItem_WithKnownValue_ExpectCorrespondingItem()
+        {
+            var mock = Mock.Create<IRandomizer>();
+
+            var array = new[]
+            {
+                "A", "B", "C"
+            };
+
+            Mock.Arrange(() => mock.NextInt(0, 3)).Returns(1);
+
+            Assert.AreEqual("B", mock.NextItem(array));
+        }
+
+        [Test]
+        public void NextItem_WithKnownValueAndMaxIndexInRange_ExpectCorrespondingItem()
+        {
+            var mock = Mock.Create<IRandomizer>();
+
+            var array = new[]
+            {
+                "A", "B", "C"
+            };
+
+            Mock.Arrange(() => mock.NextInt(0, 3)).Returns(0);
+
+            Assert.AreEqual("A", mock.NextItem(array, 2));
+        }
+
+        [Test]
+        public void NextItem_WithKnownValueUsingMinIndexAndMaxIndexInRange_ExpectCorrespondingItem()
+        {
+            var mock = Mock.Create<IRandomizer>();
+
+            var array = new[]
+            {
+                "A", "B", "C"
+            };
+
+            Mock.Arrange(() => mock.NextInt(0, 3)).Returns(2);
+
+            Assert.AreEqual("C", mock.NextItem(array, 0, 3));
+        }
+    }
+}
